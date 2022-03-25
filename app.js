3bcfdf6594e6fd3f90761d1c11c0525ddff401bc
DiegoMoralesBravo <Diegomorales1359@gmail.com>
2022-03-25 08:45:02 -0600

    se agregan views

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 var TYPES = require('tedious').TYPES;
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
 
 // Create connection to database
 var config = {
@@ -26,5 +30,23 @@ connection.on('connect', function(err) {
     console.log('Connected');
   }
 });
-
 connection.connect();
+
+app.engine('hbs', exphbs.engine({
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.get('/', (req, res) => {
+    res.render('form');
+});
+
+
+app.listen(3000, () => {
+    console.log('The web server has started on port 3000');
+});
