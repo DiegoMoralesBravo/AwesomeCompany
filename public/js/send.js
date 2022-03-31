8b6db80a4e4f899791c6f2ec3f51c7038747ddbd
DiegoMoralesBravo <Diegomorales1359@gmail.com>
2022-03-30 20:42:38 -0600

    se ajusta conexion a DB

--- public/js/send.js
+++ public/js/send.js
@@ -2,10 +2,28 @@
 $(document).ready(function () {
     var data;
     $( "#form" ).submit(function( event ) {
+
+
+
+
       data = $('#form').serialize()
+
+
+      
       console.log(data);
+
+      fetch('../app.js', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data,
+  })
+
       event.preventDefault();
+
+
+
     });
-    console.log('hola');
 
 });
